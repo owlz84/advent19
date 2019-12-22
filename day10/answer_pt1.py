1b683ed977dbb2ae0e99555356b566c4c26827a7
owlz84 <stuart@databricks.com>
2019-12-22 23:52:01 +0000

    tidied day 10 pt 1 answer

--- day10/answer_pt1.py
+++ day10/answer_pt1.py
@@ -4,6 +4,8 @@ from day10.MapReader import MapReader
 if __name__ == "__main__":
     with open("data/full_map", "r") as fh:
         asteroid_map = fh.read().splitlines()
-    map_reader = MapReader(asteroid_map)
-    map_reader.pairwise_angles()
-    print(map_reader.visible_from_best)
+    print(
+        MapReader(asteroid_map)
+        .calc_pairwise_angles()
+        .visible_from_best
+    )
