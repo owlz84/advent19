931b1c4a569049b437865ce911f888180929e254
owlz84 <stuart@databricks.com>
2019-12-23 11:49:00 +0000

    updated solution scripts

--- day10/answer_pt1.py
+++ day10/answer_pt1.py
@@ -4,8 +4,4 @@ from day10.MapReader import MapReader
 if __name__ == "__main__":
     with open("data/full_map", "r") as fh:
         asteroid_map = fh.read().splitlines()
-    print(
-        MapReader(asteroid_map)
-        .calc_pairwise_angles()
-        .visible_from_best
-    )
+    print(MapReader(asteroid_map).best_location.n_visible_asteroids)
--- day10/answer_pt2.py
+++ day10/answer_pt2.py
@@ -1,13 +1,18 @@
-from day10.MapReader import MapReader
+from math import pi
+from day10.MapReader import MapReader, LaserGun
 
 
 if __name__ == "__main__":
     with open("data/full_map", "r") as fh:
         asteroid_map = fh.read().splitlines()
+    map_reader = MapReader(asteroid_map)
     x, y = (
-        MapReader(asteroid_map)
-        .calc_pairwise_angles()
-        .calc_sweep_angles()
+        LaserGun(
+            location=map_reader.best_location,
+            asteroids=map_reader.asteroids,
+            phase=-(pi/2)
+        )
         .destroy_them_with_lasers(200)
+        .coord
     )
     print(100*x + y)
